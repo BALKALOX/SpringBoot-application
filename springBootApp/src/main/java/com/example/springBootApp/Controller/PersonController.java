4b9378bcb6b776bb53df81e5ec2411ef80fef0ce
BALKALOX <113677177+BALKALOX@users.noreply.github.com>
2023-02-08 01:17:44 +0200

    Add project

---
+++ springBootApp/src/main/java/com/example/springBootApp/Controller/PersonController.java
@@ -0,0 +1,30 @@
+package com.example.springBootApp.Controller;
+
+//import org.springframework.http.ResponseEntity;
+import com.example.springBootApp.Service.PersonService;
+import com.example.springBootApp.people.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("v1/people")
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping( path = "list")
+    public List<Person> list(){
+        return personService.list();
+    }
+
+    @RequestMapping("item/{id}")
+    public Optional<Person> show(@PathVariable Long id){
+        return personService.show(id);
+    }
+
+}
